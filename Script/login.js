711c74a854159c511e72657858c623ff0f75c64f
eklavya sanmotra <eklavyasanmotra@eklavyas-MacBook-Pro.local>
2023-07-31 22:22:55 +0530

    ‘there_you_goo’

---
+++ Script/login.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm= document.querySelector(".login-form");
+    const usersJsonFile= "sample.json";
+
+    loginForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        const emailInput= document.getElementById('Email').value;
+        const passwordInput= document.getElementById("password").value;
+        fetch(usersJsonFile)
+            .then((response)=> response.json())
+            .then((data)=> {
+                const matchingUser= data.users.find(
+                    (user) => user.email === emailInput && user.password === passwordInput
+                );
+
+                if (matchingUser) {
+                    console.log("Login successful!")
+                    alert("Login successful!");
+                  
+
+                } else {
+                    console.log("Login unsuccessful!")
+                    alert("Login unsuccessful!");
+                   
+                }
+            })
+    });
+});
